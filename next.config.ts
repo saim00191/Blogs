b083f8d6559789db214cea9f8ebb183ba0b44312
Muhammad-Talha103 <talhagp908@gmail.com>
2025-01-07 16:38:38 +0500

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['cdn.sanity.io'], // Add this line to allow images from Sanity
+    domains: ['cdn.sanity.io'], 
   },
 };
 
